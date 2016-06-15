ef2e1ba2aa65fecb1cbf935c12a0bb24b3d2242e
Katy Brimm <kbrimm@pdx.edu>
2016-06-15 14:58:29 -0700

    Initial Commit
    Initial implementation of login page. Still needs: login implementation
    as well as links to account creation and password retrieval.

---
+++ js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#loginButton').click(function() {
+        /* This function to handle user login
+         * once backend support allows
+         */
+        window.location.href = "dashboard.html";
+        return false;
+    });
+});
